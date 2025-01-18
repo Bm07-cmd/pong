410d839cb6823e8caf4ae24e5be5879f8394b7da
Bm07-cmd <bmai24598@gmail.com>
2025-01-18 11:03:42 -0800

    Update Panel.java

--- Panel.java
+++ Panel.java
@@ -1,12 +1,14 @@
-import javax.swing.JPanel;
-import java.awt.Dimension;
-import java.awt.Color;
-import java.awt.Graphics;
-import javax.swing.Timer;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import java.awt.event.KeyListener;
-import java.awt.event.KeyEvent;
+import java.*;
+// import javax.swing.JPanel;
+// import java.awt.Dimension;
+// import java.awt.Color;
+// import java.awt.Graphics;
+// import javax.swing.Timer;
+// import java.awt.event.ActionListener;
+// import java.awt.event.ActionEvent;
+// import java.awt.event.KeyListener;
+// import java.awt.event.KeyEvent;
+
 
 public class Panel extends JPanel implements ActionListener, KeyListener{
     int PANEL_WIDTH;
@@ -28,6 +30,7 @@ public class Panel extends JPanel implements ActionListener, KeyListener{
         this.p1 = new Paddle(0, 0, 12, 80);
         this.p2 = new Paddle(PANEL_WIDTH - 12, 0, 12, 80);
         this.loop = new Timer(10, this);
+        addKeyListener(this);
         loop.start();
     }
 
@@ -47,16 +50,28 @@ public class Panel extends JPanel implements ActionListener, KeyListener{
     }
 
     @Override
-    public void keyTyped(KeyEvent e){}
-
-    @Override
-    public void keyPressed(KeyEvent e){
-        System.out.println("UP ARROW PRESSED UP!");
-        
+    public void actionPerformed(ActionEvent e){
+        this.ball.update(this.PANEL_WIDTH, this.PANEL_HEIGHT);
+        repaint();
     }
 
     @Override
-    public void keyReleased(KeyEvent e){}
+    public void keyTyped(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if(e.getKeyCode() == KeyEvent.VK_UP){
+            System.out.println("up arrow");
+        }
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 
 
 }
